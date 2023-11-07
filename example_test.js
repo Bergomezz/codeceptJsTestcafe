002e91f1b439eac7e74717c1f44920821fe87d6b
Bergomezz <bernardogomezz@yahoo.com.br>
2023-11-07 12:01:48 -0300

    feature added more scenario to the test example

--- example_test.js
+++ example_test.js
@@ -1,6 +1,18 @@
 Feature('example');
 
-Scenario('test something', ({ I }) => {
+Scenario('Test 1', ({ I }) => {
+  I.amOnPage('http://example.com');
+  I.waitForElement('h1');
+  I.see('Example');
+});
+
+Scenario('Test 2', ({ I }) => {
+  I.amOnPage('http://example.com');
+  I.dontSeeElement('.login-form');
+  I.seeInCurrentUrl('example.com');
+});
+
+Scenario('Test 3', ({ I }) => {
   I.amOnPage('http://example.com');
   I.waitForElement('h1');
   I.see('Example');
